fbe55a15aa641daaf0d4152d93ba34a8be22db9d
caio-ireno <caio_ireno@hotmail.com>
2023-06-22 20:59:03 -0300

    fix: fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import "./globals.css";
 
-import { Inter } from "next/font/google";
+import { Plus_Jakarta_Sans } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Plus_Jakarta_Sans({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
