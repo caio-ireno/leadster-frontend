a28d6bf9d6c4515a99f88ab872aea993c387c7d3
caio-ireno <caio_ireno@hotmail.com>
2023-06-22 21:35:35 -0300

    feat: header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import { Header } from "@/components/Header";
+
+import RootLayout from "./layout";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1>Hello World</h1>
-    </main>
+    <RootLayout>
+      <main className={styles.main}>
+        <Header />
+      </main>
+    </RootLayout>
   );
 }
